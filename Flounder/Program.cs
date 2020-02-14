a6aa059025405d5a82305d7e573aea7faa29db0a
le0_0 <leonard.schoyen@gmail.com>
2020-02-14 19:40:29 +0100

    Another version lul

--- Flounder/Program.cs
+++ Flounder/Program.cs
@@ -2,14 +2,12 @@
 
 namespace Flounder
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine(i);
-            }
-        }
-    }
+
+  class Program
+  {
+
+    static void Main(string[] args) { Console.WriteLine("Hello World!"); }
+
+  }
+
 }
