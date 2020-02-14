61b4687ce87f31a3db730f019286034d294958a7
le0_0 <leonard.schoyen@gmail.com>
2020-02-14 19:30:35 +0100

    Initial .NET Core project

---
+++ Flounder/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Flounder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
