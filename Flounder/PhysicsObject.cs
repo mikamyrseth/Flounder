9fa733ae2133a724fbd2e56e75c31e4c897ef7bd
Mika Myrseth <mika@myrseth.org>
2020-02-15 13:59:54 +0100

    Created constructor for PhysicsObject class.

--- Flounder/PhysicsObject.cs
+++ Flounder/PhysicsObject.cs
@@ -5,7 +5,19 @@ namespace Flounder
 
     class PhysicsObject
     {
+        private string id;
+        private int x;
+        private int y;
+        private int mass;
+        private Shape shape;
         
+        public PhysicsObject(string id, int x, int y, int mass, Shape shape){
+            this.id = id;
+            this.x = x;
+            this.y = y;
+            this.mass = mass;
+            this.shape = shape;
+        }
     }
 
 }
