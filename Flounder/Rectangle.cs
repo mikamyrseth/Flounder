2724d53902506ff9d10bfa8cbbfabe3fb1a8813c
Mika Myrseth <mika@myrseth.org>
2020-02-15 14:02:59 +0100

    Changed Shape to interface and created empty Rectangle and Circle classes.

---
+++ Flounder/Rectangle.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Flounder
+{
+
+  class Rectangle:Shape
+  {
+
+    static void Main(string[] args) { Console.WriteLine("Hello World!"); }
+
+  }
+
+}
--- Flounder/Shape.cs
+++ Flounder/Shape.cs
@@ -3,7 +3,7 @@ using System;
 namespace Flounder
 {
 
-  class Shape
+  interface Shape
   {
 
     
