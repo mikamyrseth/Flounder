5ddd95728210cbafdab31ecba863587cfb46d66c
le0_0 <leonard.schoyen@gmail.com>
2020-02-15 15:18:28 +0100

    Readded Simulation class

---
+++ Flounder/Simulation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Flounder
+{
+    public class Simulation
+    {
+        private List<PhysicsObject> _physicsObjects;
+
+        public Simulation(List<PhysicsObject> physicsObjects) {
+            this._physicsObjects = physicsObjects;
+        }
+
+    }
+}
