eec6184464761da8ff31c7aca28edd5473464fc0
solveiglovseth <slovseth@gmail.com>
2020-02-15 15:00:04 +0100

    Made constructer in Simulation class

--- Flounder/Simulation.cs
+++ Flounder/Simulation.cs
@@ -1,7 +1,14 @@
-﻿namespace Flounder
+﻿using System.Collections.Generic;
+
+namespace Flounder
 {
     public class Simulation
     {
-        
+        private List<PhysicsObject> _physicsObjects;
+
+        public Simulation(List<PhysicsObject> physicsObjects) {
+            this._physicsObjects = physicsObjects;
+        }
+
     }
 }
