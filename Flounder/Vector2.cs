2deb05b14927af9d6444cee049aec13cbe517584
solveiglovseth <slovseth@gmail.com>
2020-02-19 11:04:48 +0100

    Made new struct Vector2

---
+++ Flounder/Vector2.cs
@@ -0,0 +1,13 @@
+﻿namespace Flounder
+{
+    public struct Vector2
+    {
+        private float x;
+        private float y;
+
+        public Vector2(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
