b4b487540bc5da0692622e0f185089f330c9661c
le0_0 <leonard.schoyen@gmail.com>
2020-02-21 00:34:56 +0100

    Changed field names to match project C# naming style

--- Flounder/Body.cs
+++ Flounder/Body.cs
@@ -5,24 +5,23 @@ namespace Flounder
 
     public struct Body
     {
-        private string id;
-        private int mass;
-        private IShape shape;
-        private Vector2 position;
-        private Vector2 velocity;
-        private Vector2 acceleration;
+        private string _id;
+        private int _mass;
+        private IShape _shape;
+        private Vector2 _position;
+        private Vector2 _velocity;
+        private Vector2 _acceleration;
         
         public Body(string id,  int mass, IShape shape, Vector2 position, Vector2 velocity, Vector2 acceleration) {
-            this.id = id;
-            this.mass = mass;
-            this.shape = shape;
-            this.position = position;
-            this.velocity = velocity;
-            this.acceleration = acceleration;
+            this._id = id;
+            this._mass = mass;
+            this._shape = shape;
+            this._position = position;
+            this._velocity = velocity;
+            this._acceleration = acceleration;
         }
         public Body(string id,  int mass, IShape shape, Vector2 position) :
             this(id, mass, shape, position, new Vector2(0, 0)) { }
-
         public Body(string id, int mass, IShape shape, Vector2 position, Vector2 velocity) :
             this(id, mass, shape, position, velocity, new Vector2(0, 0)) { }
     }
