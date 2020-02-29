0c5b1ae4a53f53d126241648a081a25b392d6a56
solveiglovseth <slovseth@gmail.com>
2020-02-29 11:43:05 +0100

    Implemented constant force struct

---
+++ Flounder/ConstantForce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Flounder
+{
+    public struct ConstantForce
+    {
+        private Vector2 _force;
+        private List<Body> _bodies;
+
+        public ConstantForce(Vector2 force, List<Body> bodies) {
+            this._force = force;
+            this._bodies = bodies;
+        }
+
+    }
+}
