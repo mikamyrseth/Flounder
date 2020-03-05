d76ebf907a2c34e89778e099a92bba49b2ba1c5a
le0_0 <leonard.schoyen@gmail.com>
2020-03-05 20:37:52 +0100

    Reset Program.main

--- Flounder/Flounder/Program.cs
+++ Flounder/Flounder/Program.cs
@@ -5,13 +5,8 @@ namespace Flounder
 {
     internal class Program
     {
-        private static void Main(string[] args) {
-            string inputString = InputParser.FileToJson(
-                "C:\\Users\\leona\\Documents\\Sourcetree\\flounder\\Flounder\\Flounder\\inputSchema.json"
-            );
-            dynamic input = JsonConvert.DeserializeObject(inputString);
-            // Console.WriteLine(Vector2.ParseJSON(input));
-            Console.WriteLine(Simulation.ParseJSON(input));
-        }
+
+        private static void Main(string[] args) { Console.WriteLine("Hello, World!"); }
+        
     }
 }
