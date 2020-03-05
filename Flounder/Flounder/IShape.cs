ccbb1189899f55f155c7204d9808c54d5e341fcf
le0_0 <leonard.schoyen@gmail.com>
2020-03-05 20:15:46 +0100

    Made IShape ISerializableJSON

--- Flounder/Flounder/IShape.cs
+++ Flounder/Flounder/IShape.cs
@@ -1,11 +1,30 @@
+using System.Globalization;
+using System.Linq;
+
 namespace Flounder
 {
-    public interface IShape : IIndentedLogger
+    public interface IShape : IIndentedLogger, ISerializableJSON
     {
-        public static IShape ParseJSON(dynamic JSON) {
-            if (JSON.circle != null) return Circle.ParseJSON(JSON.circle);
-            if (JSON.rectangle != null) return Rectangle.ParseJSON(JSON.rectangle);
+        
+        public static IShape ParseJSON(dynamic json) {
+            if (json.circle != null) {
+                return Circle.ParseJSON(json.circle);
+            }
+            if (json.rectangle != null) {
+                return Rectangle.ParseJSON(json.rectangle);
+            }
             return null;
         }
+
+        public static string SerializeJSON(int indent, string shapeName, string shapeJSON) {
+            string indentText = string.Concat(Enumerable.Repeat("\t", indent));
+            string text = "{\n";
+            text += indentText + $"\t\"{shapeName}\": {shapeJSON}\n";
+            text += indentText + "}";
+            return text;
+        }
+
+        new string SerializeJSON(int indent);
+
     }
 }
