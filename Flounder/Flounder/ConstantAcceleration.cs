27e1162c73b78429e513acc620856422a4e658b6
solveiglovseth <slovseth@gmail.com>
2020-03-16 12:53:04 +0100

    Created class ConstantAcceleration

---
+++ Flounder/Flounder/ConstantAcceleration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+namespace Flounder
+{
+    public class ConstantAcceleration : ISerializableJSON
+    {
+        public ConstantAcceleration(string id, Vector2 accelaration) {
+            this.Acceleration = accelaration;
+            this.ID = id;
+        }
+        public Vector2 Acceleration { get; }
+        public string ID { get; }
+        public string SerializeJSON(int indent = 0, bool singleLine = false) {
+            if (singleLine) {
+                throw new NotImplementedException();
+            }
+            string indentText = string.Concat(Enumerable.Repeat("\t", indent));
+            string text = "{\n";
+            text += indentText + $"\t\"id\": \"{this.ID}\",\n";
+            text += indentText + $"\t\"vector\": {this.Acceleration.SerializeJSON(indent + 1)}\n";
+            text += indentText + "}";
+            return text;
+        }
+        public static ConstantAcceleration ParseJSO(dynamic jso) {
+            return new ConstantAcceleration((string) jso.id, Vector2.ParseJSO(jso.vector));
+        }
+    }
+}
