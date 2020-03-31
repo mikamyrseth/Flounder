8c47c3d18966c536ee77fc2d058d97ee38724bcf
Mika Myrseth <mika@myrseth.org>
2020-04-01 00:01:37 +0200

    Added BoundingBoxComparerBySmallestX

---
+++ Flounder/Flounder/BoundingBoxComparerBySmallestX.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+namespace Flounder
+{
+  public class BoundingBoxComparer : IComparer<BoundingBoxComparer> {
+    private enum BoundingBoxAttribute {
+      BodyID,
+      MaxX,
+      MaxY,
+      MinX,
+      MinY,
+    }
+    public List<>
+  }
+}
